3681ad72edaa821134aad0d73324076b358d977b
Diego <94392280+diegolemos16@users.noreply.github.com>
2022-11-30 04:36:49 -0300

    Atualiacao da classe lista

--- src/Lista.java
+++ src/Lista.java
@@ -1,35 +1,40 @@
 public class Lista {
 
-    No comeco;
-    int tamanho;
+    private No comeco;
+
+    private int tamanho;
 
     public Lista() {
+    }
+
+    public Lista(int tamanho) {
+
         this.tamanho = tamanho;
     }
 
-// adiciona o contado na lista
-    public void addContato(Contato info) {
+    // adiciona o contado na lista
+    public void addContato(Contato contato) {
 
         No no = new No();
-        no.info = info;
-        no.proximo = comeco;
+        no.setContato(contato);
+        no.setProximo(comeco);
         comeco = no;
         tamanho++;
     }
-//deleta o contato da lista
-    public void excluirContato(Contato contato){
+
+    //deleta o contato da lista
+    public void excluirContato(Contato contato) {
         No no = comeco;
         No anterior = new No();
         No proximo = new No();
 
-        if(tamanho == 1){
+        if(tamanho == 1) {
             comeco = null;
         } else {
             while (no != null) {
                 if (contato.getCodigo() == no.getContato().getCodigo()) {
                     proximo = no.getProximo();
                     No noAnterior = comeco;
-
                     try {
                         while (noAnterior != null) {
                             if (no.getContato().getCodigo() == noAnterior.getProximo().getContato().getCodigo()) {
@@ -49,37 +54,54 @@ public class Lista {
         tamanho--;
     }
 
-
+    // pesquisa o conta por codigo (nao eh o que queremos)
     public Contato pesquisarContato(int codigo) {
         No no = comeco;
-        while (no != null){
-            if (no.contato.codigo == codigo){
-                return no.info;
+        while (no != null) {
+            if (no.getContato().getCodigo() == codigo) {
+                return no.getContato();
             }
-            no = no.proximo;
+            no = no.getProximo();
         }
         return null;
     }
 
+    //pesquisa o contato por nome
     public Contato pesquisarNome(String nome) {
         No no = comeco;
-        while (no != null){
-            if (no.equals(no.contato.nome)){
-                return no.info;
+        while (no != null) {
+            if (nome.equalsIgnoreCase(no.getContato().getNome())) {
+                return no.getContato();
             }
-            no = no.proximo;
+            no = no.getProximo();
         }
-    return null;
+        return null;
     }
 
-
+    @Override
     public String toString() {
         String out = "";
         No no = comeco;
-        while (no != null){
-            out += no.info + " ";
-            no = no.proximo;
+        while (no != null) {
+            out += no.getContato() + " ";
+            no = no.getProximo();
         }
         return out;
+    }
+
+    public No getComeco() {
+        return comeco;
+    }
+
+    public void setComeco(No comeco) {
+        this.comeco = comeco;
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(int tamanho) {
+        this.tamanho = tamanho;
     }
 }
