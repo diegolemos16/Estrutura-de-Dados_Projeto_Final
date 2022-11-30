7e43ea8a7e7d0e8c63ab12d83e265dca9f14045f
Diego <94392280+diegolemos16@users.noreply.github.com>
2022-11-30 04:20:16 -0300

    Ajustes de espaços
    Somente mudados os espaçamentos

--- src/Opcoes.java
+++ src/Opcoes.java
@@ -1,19 +1,17 @@
+
 public class Opcoes {
 
-    public Opcoes(){
+    public Opcoes() {
 
     }
 
-        public static void opcoesMenu() {
+    public static void opcoesMenu() {
 
-            System.out.println("------------ MENU INICIAL ------------");
-            System.out.println(" 1. Adicionar contato");
-            System.out.println(" 2. Excluir contato");
-            System.out.println(" 3. Pesquisar contato");
-            System.out.println(" 4. Ver todos");
-            System.out.println(" 5. Sair");
-        }
-
-
+        System.out.println("------------ MENU INICIAL ------------");
+        System.out.println(" 1. Adicionar contato");
+        System.out.println(" 2. Excluir contato");
+        System.out.println(" 3. Pesquisar contato");
+        System.out.println(" 4. Ver todos");
+        System.out.println(" 5. Sair");
+    }
 }
-
