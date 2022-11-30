a53e9ff52ef49f15a1d3bc3c877fee392b038505
Diego <94392280+diegolemos16@users.noreply.github.com>
2022-11-30 04:31:09 -0300

    Ajustes na classe Hash
    Inclusao do Try Catch, inclusao dos metodos hash excluir e pesquisar pornome, e ajuste nos outros

--- src/Hash.java
+++ src/Hash.java
@@ -1,46 +1,59 @@
+
 public class Hash {
 
-    int tamTabela;
-    Lista [] vetor;
+    private int tamTabela;
+
+    private Lista[] vetor;
 
     public Hash(int tamTabela) {
         this.tamTabela = tamTabela;
         vetor = new Lista[tamTabela];
-        for (int i = 0; i < tamTabela; i++){
+        for (int i = 0; i < tamTabela; i++) {
             vetor[i] = new Lista();
-                    }
+        }
     }
 
-    public void addContato(Contato contato){
-        int chave = contato.codigo % tamTabela;
+    public void addContato(Contato contato) {
+        int chave = contato.getCodigo() % tamTabela;
         vetor[chave].addContato(contato);
-
     }
 
-
-    // Diego
-    public void excluirContato(String nome){
-
+    public void excluirContato(String nome) {
+        try {
+            boolean b = pesquisarPorNome(nome) != null;
+            {
+                Contato contato = pesquisarPorNome(nome);
+                int chave = contato.getCodigo() % tamTabela;
+                vetor[chave].excluirContato(contato);
+            }
+        }catch (NullPointerException g){
+                System.out.println("Impossível deletar, contato não encontrado!");
+            }
+        }
+    
+    //funcao 'pesquisarContato' nao sera chamada na classe Agenda
+    public Contato pesquisarContato(int codigo) {
+        Contato retorno = vetor[codigo % tamTabela].pesquisarContato(codigo);
+        System.out.println(retorno);
+        return retorno;
     }
 
-    public Contato pesquisarContato(int codigo){
-        Contato resultado = vetor[codigo % tamTabela].pesquisarContato(codigo);
-        return resultado;
+    public Contato pesquisarPorNome(String nome) {
+        for (int i = 0; i < tamTabela; i++) {
+            if (vetor[i].pesquisarNome(nome) != null) {
+                return vetor[i].pesquisarNome(nome);
+            };
+        }
+        System.out.println("Contato nao encontrado");
+        return null;
     }
 
-    // Diego
-   public Contato pesquisarPorNome(String nome){
+    public void exibirAgenda() {
+        System.out.println(this);
+    }
 
-       return null;
-   }
-
-   void exibirAgenda(){
-       System.out.println(this);
-
-   }
-
-
-    public String toString(){
+    @Override
+    public String toString() {
         String out = "";
         for (int i = 0; i < tamTabela; i++) {
             out += "" + i + ": ";
